335d88928cbba5ff115c7a1f10bac02b46b2a4d6
Fangdun Cai (Fundon) <cfddream@gmail.com>
2016-09-23 16:56:30 +0800

    util: improve sendError

--- test/engine/on-error.js
+++ test/engine/on-error.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import test from 'ava'
 import request from 'request-promise'
 import Engine from '../../lib/engine'
@@ -28,6 +29,47 @@ test('handle catch should response and throws 404', async t => {
 
   app.use(() => {
     throw new HTTPError(404, 'Not found')
+  })
+
+  app.on('error', err => {
+    t.true(err !== null)
+  })
+
+  const url = await listen(app)
+  try {
+    await request(url)
+  } catch (err) {
+    t.true(err.statusCode === 404)
+  }
+})
+
+test('should handle errors when no content status', async t => {
+  const app = new Engine()
+
+  app.use(({ res }) => {
+    res.status = 204
+    res.body = fs.createReadStream('does not exist')
+  })
+
+  app.on('error', err => {
+    t.true(err !== null)
+  })
+
+  const url = await listen(app)
+  try {
+    await request(url)
+  } catch (err) {
+    t.true(err.statusCode === 204)
+  }
+})
+
+test('should handle all intermediate stream body errors', async t => {
+  const app = new Engine()
+
+  app.use(({ res }) => {
+    res.body = fs.createReadStream('does not exist')
+    res.body = fs.createReadStream('does not exist')
+    res.body = fs.createReadStream('does not exist')
   })
 
   app.on('error', err => {
