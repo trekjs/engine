e188e31a784a4ba5fab4c57d1d74e43f73d713ae
Fangdun Cai <cfddream@gmail.com>
2017-02-14 00:41:49 +0800

    request: fix malformed content-type header causing exception on charset get

--- test/request/charset.js
+++ test/request/charset.js
@@ -21,3 +21,9 @@ test('with a charset', t => {
   req.header['content-type'] = 'text/plain; charset=utf-8'
   t.is(req.charset, 'utf-8')
 })
+
+test('should return "" if content-type is invalid', t => {
+  const req = t.context
+  req.header['content-type'] = 'application/json; application/text; charset=utf-8'
+  t.is(req.charset, '')
+})
